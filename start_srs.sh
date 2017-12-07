80f011317d4efc51223fa9e0fe104f9002415f12
Zhou Weitong <xml@live.com>
2017-12-07 13:01:28 +0800

    12.07.1

--- start_srs.sh
+++ start_srs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /root/shell
-nohup ./srs.sh > /root/logs/srs_log/srs_$(date "+%Y-%m-%d-%H-%M-%S").log 2&>1 &
-nohup ./mse.sh > /root/logs/mse_log/mse_$(date "+%Y-%m-%d-%H-%M-%S").log 2&>1 &
+./srs.sh
+nohup ./mse.sh > /root/logs/mse_log/mse_$(date "+%Y-%m-%d-%H-%M-%S").log 2&>1
 while true;
 do sleep 1;
 done;
